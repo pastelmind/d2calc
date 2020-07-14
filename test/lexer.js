bd9d0e0128e0d2f6ce75849f29fc59b59116e8c6
pastelmind <keepyourhonor@gmail.com>
2020-07-14 22:37:03 +0900

    test(lexer): Rewrite lexer tests

--- test/lexer.js
+++ test/lexer.js
@@ -8,14 +8,16 @@ const assert = require("assert").strict;
 const tokenize = require("../src/lexer.js");
 
 const {
+  AdditiveOperatorToken,
   ClosingParenthesisToken,
   ColonToken,
   CommaToken,
-  DotToken,
+  ComparisonOperatorToken,
+  DotCodeToken,
   IdentifierToken,
+  MultiplicativeOperatorToken,
   NumberToken,
   OpeningParenthesisToken,
-  OperatorToken,
   QuestionMarkToken,
   ReferenceToken,
 } = tokenize;
@@ -31,23 +33,39 @@ describe("tokenize()", () => {
   });
 
   it("should tokenize numbers", () => {
-    assert.deepStrictEqual(tokenize("0"), [new NumberToken(0, 0)]);
-    assert.deepStrictEqual(tokenize("369"), [new NumberToken(0, 369)]);
+    assert.deepStrictEqual(tokenize("0"), [new NumberToken(0, "0", 0)]);
+    assert.deepStrictEqual(tokenize("369"), [new NumberToken(0, "369", 369)]);
   });
 
   it("should tokenize operators", () => {
-    assert.deepStrictEqual(tokenize("+"), [new OperatorToken(0, "+")]);
-    assert.deepStrictEqual(tokenize("-"), [new OperatorToken(0, "-")]);
-    assert.deepStrictEqual(tokenize("*"), [new OperatorToken(0, "*")]);
-    assert.deepStrictEqual(tokenize("/"), [new OperatorToken(0, "/")]);
+    assert.deepStrictEqual(tokenize("+"), [new AdditiveOperatorToken(0, "+")]);
+    assert.deepStrictEqual(tokenize("-"), [new AdditiveOperatorToken(0, "-")]);
+    assert.deepStrictEqual(tokenize("*"), [
+      new MultiplicativeOperatorToken(0, "*"),
+    ]);
+    assert.deepStrictEqual(tokenize("/"), [
+      new MultiplicativeOperatorToken(0, "/"),
+    ]);
 
-    assert.deepStrictEqual(tokenize("=="), [new OperatorToken(0, "==")]);
-    assert.deepStrictEqual(tokenize("!="), [new OperatorToken(0, "!=")]);
+    assert.deepStrictEqual(tokenize("=="), [
+      new ComparisonOperatorToken(0, "=="),
+    ]);
+    assert.deepStrictEqual(tokenize("!="), [
+      new ComparisonOperatorToken(0, "!="),
+    ]);
 
-    assert.deepStrictEqual(tokenize("<"), [new OperatorToken(0, "<")]);
-    assert.deepStrictEqual(tokenize(">"), [new OperatorToken(0, ">")]);
-    assert.deepStrictEqual(tokenize("<="), [new OperatorToken(0, "<=")]);
-    assert.deepStrictEqual(tokenize(">="), [new OperatorToken(0, ">=")]);
+    assert.deepStrictEqual(tokenize("<"), [
+      new ComparisonOperatorToken(0, "<"),
+    ]);
+    assert.deepStrictEqual(tokenize(">"), [
+      new ComparisonOperatorToken(0, ">"),
+    ]);
+    assert.deepStrictEqual(tokenize("<="), [
+      new ComparisonOperatorToken(0, "<="),
+    ]);
+    assert.deepStrictEqual(tokenize(">="), [
+      new ComparisonOperatorToken(0, ">="),
+    ]);
   });
 
   it("should tokenize parentheses", () => {
@@ -56,7 +74,6 @@ describe("tokenize()", () => {
   });
 
   it("should tokenize symbols", () => {
-    assert.deepStrictEqual(tokenize("."), [new DotToken(0)]);
     assert.deepStrictEqual(tokenize(","), [new CommaToken(0)]);
     assert.deepStrictEqual(tokenize("?"), [new QuestionMarkToken(0)]);
     assert.deepStrictEqual(tokenize(":"), [new ColonToken(0)]);
@@ -69,30 +86,40 @@ describe("tokenize()", () => {
   });
 
   it("should tokenize references", () => {
-    assert.deepStrictEqual(tokenize("''"), [new ReferenceToken(0, "")]);
+    assert.deepStrictEqual(tokenize("''"), [new ReferenceToken(0, "''", "")]);
     assert.deepStrictEqual(tokenize("'skill name'"), [
-      new ReferenceToken(0, "skill name"),
+      new ReferenceToken(0, "'skill name'", "skill name"),
+    ]);
+  });
+
+  it("should tokenize dot codes", () => {
+    assert.deepStrictEqual(tokenize(".code"), [
+      new DotCodeToken(0, ".code", "code"),
     ]);
   });
 
   it("should ignore whitespace around tokens", () => {
-    assert.deepStrictEqual(tokenize(" 1 "), [new NumberToken(1, 1)]);
-    assert.deepStrictEqual(tokenize("  +    "), [new OperatorToken(2, "+")]);
-    assert.deepStrictEqual(tokenize("  ==  "), [new OperatorToken(2, "==")]);
+    assert.deepStrictEqual(tokenize(" 1 "), [new NumberToken(1, "1", 1)]);
+    assert.deepStrictEqual(tokenize("  +    "), [
+      new AdditiveOperatorToken(2, "+"),
+    ]);
+    assert.deepStrictEqual(tokenize("  ==  "), [
+      new ComparisonOperatorToken(2, "=="),
+    ]);
     assert.deepStrictEqual(tokenize("  (  )  "), [
       new OpeningParenthesisToken(2),
       new ClosingParenthesisToken(5),
     ]);
     assert.deepStrictEqual(tokenize(" lvl > 2 ? lvl + 3 : 0 "), [
       new IdentifierToken(1, "lvl"),
-      new OperatorToken(5, ">"),
-      new NumberToken(7, 2),
+      new ComparisonOperatorToken(5, ">"),
+      new NumberToken(7, "2", 2),
       new QuestionMarkToken(9),
       new IdentifierToken(11, "lvl"),
-      new OperatorToken(15, "+"),
-      new NumberToken(17, 3),
+      new AdditiveOperatorToken(15, "+"),
+      new NumberToken(17, "3", 3),
       new ColonToken(19),
-      new NumberToken(21, 0),
+      new NumberToken(21, "0", 0),
     ]);
   });
 });
