68cee9bea3c8b6eb21f85ca7064b63dee9c855a4
pastelmind <keepyourhonor@gmail.com>
2020-07-16 16:14:35 +0900

    test(lexer): Seperate each code into test case
    Separate each test code into individual test cases, so that faulty tests
    can be debugged separately.

--- test/lexer.js
+++ test/lexer.js
@@ -18,77 +18,89 @@ const {
   OperatorToken,
   QuestionMarkToken,
   ReferenceToken,
+  Token,
 } = tokenize;
 
+/**
+ * Verifies that the given code is tokenized to the given token sequence.
+ * Helper method for running Mocha tests.
+ *
+ * @param {string} code
+ * @param {InstanceType<Token>[]} tokens
+ */
+function itTokenizesTo(code, tokens) {
+  it(`Test "${code}"`, () => {
+    assert.deepStrictEqual(tokenize(code), tokens);
+  });
+}
+
 describe("tokenize()", () => {
-  it("should tokenize empty string to nothing", () => {
-    assert.deepStrictEqual(tokenize(""), []);
+  describe("should tokenize empty string to nothing", () => {
+    itTokenizesTo("", []);
   });
 
-  it("should tokenize blank string to nothing", () => {
-    assert.deepStrictEqual(tokenize(" "), []);
-    assert.deepStrictEqual(tokenize("    "), []);
+  describe("should tokenize blank string to nothing", () => {
+    itTokenizesTo(" ", []);
+    itTokenizesTo("    ", []);
   });
 
-  it("should tokenize numbers", () => {
-    assert.deepStrictEqual(tokenize("0"), [new NumberToken(0, "0", 0)]);
-    assert.deepStrictEqual(tokenize("369"), [new NumberToken(0, "369", 369)]);
+  describe("should tokenize numbers", () => {
+    itTokenizesTo("0", [new NumberToken(0, "0", 0)]);
+    itTokenizesTo("369", [new NumberToken(0, "369", 369)]);
   });
 
-  it("should tokenize operators", () => {
-    assert.deepStrictEqual(tokenize("+"), [new OperatorToken(0, "+")]);
-    assert.deepStrictEqual(tokenize("-"), [new OperatorToken(0, "-")]);
-    assert.deepStrictEqual(tokenize("*"), [new OperatorToken(0, "*")]);
-    assert.deepStrictEqual(tokenize("/"), [new OperatorToken(0, "/")]);
+  describe("should tokenize operators", () => {
+    itTokenizesTo("+", [new OperatorToken(0, "+")]);
+    itTokenizesTo("-", [new OperatorToken(0, "-")]);
+    itTokenizesTo("*", [new OperatorToken(0, "*")]);
+    itTokenizesTo("/", [new OperatorToken(0, "/")]);
 
-    assert.deepStrictEqual(tokenize("=="), [new OperatorToken(0, "==")]);
-    assert.deepStrictEqual(tokenize("!="), [new OperatorToken(0, "!=")]);
+    itTokenizesTo("==", [new OperatorToken(0, "==")]);
+    itTokenizesTo("!=", [new OperatorToken(0, "!=")]);
 
-    assert.deepStrictEqual(tokenize("<"), [new OperatorToken(0, "<")]);
-    assert.deepStrictEqual(tokenize(">"), [new OperatorToken(0, ">")]);
-    assert.deepStrictEqual(tokenize("<="), [new OperatorToken(0, "<=")]);
-    assert.deepStrictEqual(tokenize(">="), [new OperatorToken(0, ">=")]);
+    itTokenizesTo("<", [new OperatorToken(0, "<")]);
+    itTokenizesTo(">", [new OperatorToken(0, ">")]);
+    itTokenizesTo("<=", [new OperatorToken(0, "<=")]);
+    itTokenizesTo(">=", [new OperatorToken(0, ">=")]);
   });
 
-  it("should tokenize parentheses", () => {
-    assert.deepStrictEqual(tokenize("("), [new OpeningParenthesisToken(0)]);
-    assert.deepStrictEqual(tokenize(")"), [new ClosingParenthesisToken(0)]);
+  describe("should tokenize parentheses", () => {
+    itTokenizesTo("(", [new OpeningParenthesisToken(0)]);
+    itTokenizesTo(")", [new ClosingParenthesisToken(0)]);
   });
 
-  it("should tokenize symbols", () => {
-    assert.deepStrictEqual(tokenize(","), [new CommaToken(0)]);
-    assert.deepStrictEqual(tokenize("?"), [new QuestionMarkToken(0)]);
-    assert.deepStrictEqual(tokenize(":"), [new ColonToken(0)]);
+  describe("should tokenize symbols", () => {
+    itTokenizesTo(",", [new CommaToken(0)]);
+    itTokenizesTo("?", [new QuestionMarkToken(0)]);
+    itTokenizesTo(":", [new ColonToken(0)]);
   });
 
-  it("should tokenize identifiers", () => {
-    assert.deepStrictEqual(tokenize("min"), [new IdentifierToken(0, "min")]);
-    assert.deepStrictEqual(tokenize("ln12"), [new IdentifierToken(0, "ln12")]);
-    assert.deepStrictEqual(tokenize("Foo3"), [new IdentifierToken(0, "Foo3")]);
+  describe("should tokenize identifiers", () => {
+    itTokenizesTo("min", [new IdentifierToken(0, "min")]);
+    itTokenizesTo("ln12", [new IdentifierToken(0, "ln12")]);
+    itTokenizesTo("Foo3", [new IdentifierToken(0, "Foo3")]);
   });
 
-  it("should tokenize references", () => {
-    assert.deepStrictEqual(tokenize("''"), [new ReferenceToken(0, "''", "")]);
-    assert.deepStrictEqual(tokenize("'skill name'"), [
+  describe("should tokenize references", () => {
+    itTokenizesTo("''", [new ReferenceToken(0, "''", "")]);
+    itTokenizesTo("'skill name'", [
       new ReferenceToken(0, "'skill name'", "skill name"),
     ]);
   });
 
-  it("should tokenize dot codes", () => {
-    assert.deepStrictEqual(tokenize(".code"), [
-      new DotCodeToken(0, ".code", "code"),
-    ]);
+  describe("should tokenize dot codes", () => {
+    itTokenizesTo(".code", [new DotCodeToken(0, ".code", "code")]);
   });
 
-  it("should ignore whitespace around tokens", () => {
-    assert.deepStrictEqual(tokenize(" 1 "), [new NumberToken(1, "1", 1)]);
-    assert.deepStrictEqual(tokenize("  +    "), [new OperatorToken(2, "+")]);
-    assert.deepStrictEqual(tokenize("  ==  "), [new OperatorToken(2, "==")]);
-    assert.deepStrictEqual(tokenize("  (  )  "), [
+  describe("should ignore whitespace around tokens", () => {
+    itTokenizesTo(" 1 ", [new NumberToken(1, "1", 1)]);
+    itTokenizesTo("  +    ", [new OperatorToken(2, "+")]);
+    itTokenizesTo("  ==  ", [new OperatorToken(2, "==")]);
+    itTokenizesTo("  (  )  ", [
       new OpeningParenthesisToken(2),
       new ClosingParenthesisToken(5),
     ]);
-    assert.deepStrictEqual(tokenize(" lvl > 2 ? lvl + 3 : 0 "), [
+    itTokenizesTo(" lvl > 2 ? lvl + 3 : 0 ", [
       new IdentifierToken(1, "lvl"),
       new OperatorToken(5, ">"),
       new NumberToken(7, "2", 2),
