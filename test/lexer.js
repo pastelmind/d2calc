606d4504180c58d102edf7906b3a3fb9caae41f0
pastelmind <keepyourhonor@gmail.com>
2020-07-16 16:06:25 +0900

    test(lexer): Update tests to use OperatorToken

--- test/lexer.js
+++ test/lexer.js
@@ -8,16 +8,14 @@ const assert = require("assert").strict;
 const tokenize = require("../src/lexer.js");
 
 const {
-  AdditiveOperatorToken,
   ClosingParenthesisToken,
   ColonToken,
   CommaToken,
-  ComparisonOperatorToken,
   DotCodeToken,
   IdentifierToken,
-  MultiplicativeOperatorToken,
   NumberToken,
   OpeningParenthesisToken,
+  OperatorToken,
   QuestionMarkToken,
   ReferenceToken,
 } = tokenize;
@@ -38,34 +36,18 @@ describe("tokenize()", () => {
   });
 
   it("should tokenize operators", () => {
-    assert.deepStrictEqual(tokenize("+"), [new AdditiveOperatorToken(0, "+")]);
-    assert.deepStrictEqual(tokenize("-"), [new AdditiveOperatorToken(0, "-")]);
-    assert.deepStrictEqual(tokenize("*"), [
-      new MultiplicativeOperatorToken(0, "*"),
-    ]);
-    assert.deepStrictEqual(tokenize("/"), [
-      new MultiplicativeOperatorToken(0, "/"),
-    ]);
+    assert.deepStrictEqual(tokenize("+"), [new OperatorToken(0, "+")]);
+    assert.deepStrictEqual(tokenize("-"), [new OperatorToken(0, "-")]);
+    assert.deepStrictEqual(tokenize("*"), [new OperatorToken(0, "*")]);
+    assert.deepStrictEqual(tokenize("/"), [new OperatorToken(0, "/")]);
 
-    assert.deepStrictEqual(tokenize("=="), [
-      new ComparisonOperatorToken(0, "=="),
-    ]);
-    assert.deepStrictEqual(tokenize("!="), [
-      new ComparisonOperatorToken(0, "!="),
-    ]);
+    assert.deepStrictEqual(tokenize("=="), [new OperatorToken(0, "==")]);
+    assert.deepStrictEqual(tokenize("!="), [new OperatorToken(0, "!=")]);
 
-    assert.deepStrictEqual(tokenize("<"), [
-      new ComparisonOperatorToken(0, "<"),
-    ]);
-    assert.deepStrictEqual(tokenize(">"), [
-      new ComparisonOperatorToken(0, ">"),
-    ]);
-    assert.deepStrictEqual(tokenize("<="), [
-      new ComparisonOperatorToken(0, "<="),
-    ]);
-    assert.deepStrictEqual(tokenize(">="), [
-      new ComparisonOperatorToken(0, ">="),
-    ]);
+    assert.deepStrictEqual(tokenize("<"), [new OperatorToken(0, "<")]);
+    assert.deepStrictEqual(tokenize(">"), [new OperatorToken(0, ">")]);
+    assert.deepStrictEqual(tokenize("<="), [new OperatorToken(0, "<=")]);
+    assert.deepStrictEqual(tokenize(">="), [new OperatorToken(0, ">=")]);
   });
 
   it("should tokenize parentheses", () => {
@@ -100,23 +82,19 @@ describe("tokenize()", () => {
 
   it("should ignore whitespace around tokens", () => {
     assert.deepStrictEqual(tokenize(" 1 "), [new NumberToken(1, "1", 1)]);
-    assert.deepStrictEqual(tokenize("  +    "), [
-      new AdditiveOperatorToken(2, "+"),
-    ]);
-    assert.deepStrictEqual(tokenize("  ==  "), [
-      new ComparisonOperatorToken(2, "=="),
-    ]);
+    assert.deepStrictEqual(tokenize("  +    "), [new OperatorToken(2, "+")]);
+    assert.deepStrictEqual(tokenize("  ==  "), [new OperatorToken(2, "==")]);
     assert.deepStrictEqual(tokenize("  (  )  "), [
       new OpeningParenthesisToken(2),
       new ClosingParenthesisToken(5),
     ]);
     assert.deepStrictEqual(tokenize(" lvl > 2 ? lvl + 3 : 0 "), [
       new IdentifierToken(1, "lvl"),
-      new ComparisonOperatorToken(5, ">"),
+      new OperatorToken(5, ">"),
       new NumberToken(7, "2", 2),
       new QuestionMarkToken(9),
       new IdentifierToken(11, "lvl"),
-      new AdditiveOperatorToken(15, "+"),
+      new OperatorToken(15, "+"),
       new NumberToken(17, "3", 3),
       new ColonToken(19),
       new NumberToken(21, "0", 0),
