5df949d19ae4bb157c60ad4e374ffccd01eb90b5
pastelmind <keepyourhonor@gmail.com>
2020-07-19 00:34:00 +0900

    fixup: feat(interp): Export CachedInterpreter in index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 export { default as interpret } from "./src/interpreter.js";
-export { default as CachedInterpreter } from "./src/cached-interpreter.js";
+export { CachedInterpreter } from "./src/cached-interpreter.js";
 export * from "./src/errors.js";
