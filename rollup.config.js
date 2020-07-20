01f0ea913f5dcf9f90216ffafbf5efd1fbe0bdef
pastelmind <keepyourhonor@gmail.com>
2020-07-21 00:24:24 +0900

    build: Create template for shared output options

--- rollup.config.js
+++ rollup.config.js
@@ -1,54 +1,42 @@
 import { terser } from "rollup-plugin-terser";
 import pkg from "./package.json";
 
+/**
+ * Shared options for all output bundles
+ * @type {import("rollup").OutputOptions}
+ */
+const outputOptionsBase = {
+  name: pkg.name, // For UMD bundles
+  plugins: [
+    terser({
+      mangle: {
+        // Preserve exception class names
+        keep_classnames: true,
+      },
+    }),
+  ],
+  sourcemap: true,
+};
+
 /** @type {import("rollup").RollupOptions} */
 const options = {
   // Use the ESM entrypoint script as the bundle input
   input: pkg.exports.import,
   output: [
-    {
+    Object.assign({}, outputOptionsBase, {
       file: `dist/${pkg.name}.umd.cjs`,
       format: "umd",
-      name: pkg.name,
-      plugins: [
-        terser({
-          mangle: {
-            // Preserve exception class names
-            keep_classnames: true,
-          },
-        }),
-      ],
-      sourcemap: true,
-    },
-    {
+    }),
+    Object.assign({}, outputOptionsBase, {
       // For Node.js 10.x, which does not recognize the '*.cjs' extension when
       // using '--experimental-modules'
       file: `dist/${pkg.name}.umd.js`,
       format: "umd",
-      name: pkg.name,
-      plugins: [
-        terser({
-          mangle: {
-            // Preserve exception class names
-            keep_classnames: true,
-          },
-        }),
-      ],
-      sourcemap: true,
-    },
-    {
+    }),
+    Object.assign({}, outputOptionsBase, {
       file: `dist/${pkg.name}.esm.js`,
       format: "esm",
-      plugins: [
-        terser({
-          mangle: {
-            // Preserve exception class names
-            keep_classnames: true,
-          },
-        }),
-      ],
-      sourcemap: true,
-    },
+    }),
   ],
 };
 
