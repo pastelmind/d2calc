76eede33fb8666f8c5065963b13d7b43e88706d1
pastelmind <keepyourhonor@gmail.com>
2020-07-21 00:16:03 +0900

    build: Properly type check Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
 import { terser } from "rollup-plugin-terser";
 import pkg from "./package.json";
 
-export default {
+/** @type {import("rollup").RollupOptions} */
+const options = {
   // Use the ESM entrypoint script as the bundle input
   input: pkg.exports.import,
   output: [
@@ -9,12 +10,14 @@ export default {
       file: `dist/${pkg.name}.umd.cjs`,
       format: "umd",
       name: pkg.name,
-      plugins: terser({
-        mangle: {
-          // Preserve exception class names
-          keep_classnames: true,
-        },
-      }),
+      plugins: [
+        terser({
+          mangle: {
+            // Preserve exception class names
+            keep_classnames: true,
+          },
+        }),
+      ],
       sourcemap: true,
     },
     {
@@ -23,24 +26,30 @@ export default {
       file: `dist/${pkg.name}.umd.js`,
       format: "umd",
       name: pkg.name,
-      plugins: terser({
-        mangle: {
-          // Preserve exception class names
-          keep_classnames: true,
-        },
-      }),
+      plugins: [
+        terser({
+          mangle: {
+            // Preserve exception class names
+            keep_classnames: true,
+          },
+        }),
+      ],
       sourcemap: true,
     },
     {
       file: `dist/${pkg.name}.esm.js`,
       format: "esm",
-      plugins: terser({
-        mangle: {
-          // Preserve exception class names
-          keep_classnames: true,
-        },
-      }),
+      plugins: [
+        terser({
+          mangle: {
+            // Preserve exception class names
+            keep_classnames: true,
+          },
+        }),
+      ],
       sourcemap: true,
     },
   ],
 };
+
+export default options;
